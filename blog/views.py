6d38cdb2fcd58f08963d94de563c8282050425fd
John Pfuntner -X (jpfuntne - THE SELECT GROUP LLC at Cisco) <jpfuntne@cisco.com>
2018-01-29 13:43:26 -0500

    View post detail, see all posts
    Adding a page to show the detail of a particular post (view only).  I also changed the original view so that it would return all posts, not just published posts.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
-    # get posts that have been published, sorted by their publication date
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # get posts sorted by their publication date
+
+    #
+    #   The original view excluded posts that were not published so I removed the filter so all posts are returned
+    #
+    #     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #
+
+    posts = Post.objects.order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+  post = get_object_or_404(Post, pk=pk)
+  return render(request, "blog/post_detail.html", {"post": post})
